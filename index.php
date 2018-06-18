275a8158c4eca9b53d81e437cfa87576e17d84e0
LopesDuarte <lopesduarte94@gmail.com>
2018-06-18 11:33:53 +0100

    ultimo update de teste

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <!DOCTYPE html>
 <html lang="">
+
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TESTE de GIT</title>
 </head>
 
 <body>
-  
+    <p> Hello World</p>
+    <ul>
+        <li>Lets go</li>
+        <li>Use GIT</li>
+        <li>For the first time!!</li>
+        <li>EASY</li>
+        <li>GET REKT</li>
+        <li>LUIGI BAIT</li>
+    </ul>
 </body>
+
 </html>
